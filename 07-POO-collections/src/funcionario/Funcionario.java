d576e2425a0f4d9d3afc5330463ae89eca6578b4
Vanilson Burégio <vaab@cin.ufpe.br>
2017-02-23 22:33:21 -0300

    Added Funcionario

--- 07-POO-collections/src/funcionario/Funcionario.java
+++ 07-POO-collections/src/funcionario/Funcionario.java
@@ -1,5 +1,34 @@
 package funcionario;
 
 public class Funcionario {
-
+	private Long id;
+	private String nome;
+	private Long idade;
+	private Double salario;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Long getIdade() {
+		return idade;
+	}
+	public void setIdade(Long idade) {
+		this.idade = idade;
+	}
+	public Double getSalario() {
+		return salario;
+	}
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
+	
+	
 }
